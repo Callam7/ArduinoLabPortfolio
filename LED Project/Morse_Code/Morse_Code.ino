852b0e0bb04de0ed8e4c6186fcbc5ecc53abb44f
Callam Jackson <jackcj1@student.op.ac.nz>
2019-03-26 12:34:43 +1300

    edited documentation as well as code itself

--- LED Project/Morse_Code/Morse_Code.ino
+++ LED Project/Morse_Code/Morse_Code.ino
@@ -1,5 +1,4 @@
 // Much of the code was constructed based around this source https://www.instructables.com/id/Arduino-Morse-Code/, however was heavily modified in favour of more cleaner code.
-// I have modified it to include more than one LED and to use a buzzer as well.
 // I also followed along with the international morse code alphabet https://en.wikipedia.org/wiki/Morse_code#/media/File:International_Morse_Code.svg to help with setting up and syncing the
 // multiple LEDs.
 char input;
